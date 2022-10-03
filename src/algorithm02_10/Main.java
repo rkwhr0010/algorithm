7b00729308b6bcaf46459b002a87037c98054c58
kks <rkwhr0010@naver.com>
2022-10-03 23:05:12 +0900

    봉우리 커밋

--- src/algorithm02_10/Main.java
+++ src/algorithm02_10/Main.java
@@ -1,31 +1,39 @@
 package algorithm02_10;
 
-import java.util.*;
-import java.util.stream.IntStream;
-class Main {	
 
-	private static boolean isTop(int[][]arr, int i, int j) {
-		
-		return arr[i][j] < arr[i - 1][j] ? false
-		     : arr[i][j] < arr[i + 1][j] ? false
-		     : arr[i][j] < arr[i][j - 1] ? false 
-		     : arr[i][j] < arr[i][j + 1] ? false : true;
+import java.util.*;
+class Main {	
+	int[] dx={-1, 0, 1, 0};
+	int[] dy={0, 1, 0, -1};
+	public int solution(int n, int[][] arr){
+		int answer=0;
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				boolean flag=true;
+				for(int k=0; k<4; k++){
+					int nx=i+dx[k];
+					int ny=j+dy[k];
+					if(nx>=0 && nx<n && ny>=0 && ny<n && arr[nx][ny]>=arr[i][j]){
+						flag=false;
+						break;
+					}
+				}
+				if(flag) answer++;
+			}
+		}
+		return answer;
 	}
-	
-	
+
 	public static void main(String[] args){
-		final int SIZE = 10;
-		final int MAX = 20;
-		int[] array = IntStream.range(0, SIZE)
-		         .toArray();
-		int[][] arr = Arrays.stream(array)
-              .mapToObj(n->{
-            	  return new Random().ints(SIZE, 0, MAX)
-            			             .toArray();
-              }).toArray(int[][]::new);
-		
-		
-		
-		
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int[][] arr=new int[n][n];
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				arr[i][j]=kb.nextInt();
+			}
+		}
+		System.out.print(T.solution(n, arr));
 	}
 }
